378410a2c1f96e2b00dab08ab003a6fba7a88b6e
RevayDev <revaydev17@gmail.com>
2024-10-07 02:02:49 +0200

    Eliminando archivo grande

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './assets/stylesheets/App.scss'
 import Inicio  from './assets/pages/Inicio'
 import Nav from './Nav.jsx'
-import Error from './assets/pages/Error'
+import Error from './assets/pages/404.jsx'
 import Proyectos from './assets/pages/Proyectos'
 import { Routes, Route } from 'react-router-dom'
 
